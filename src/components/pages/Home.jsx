37afdecbf4978b66395557458b59b8a63269acbe
Jarilton <jariltonpereira02@hotmail.com>
2021-12-09 11:45:49 -0300

    Criando a pagina Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButtoon from '../layout/LinkButton'
+
 function Home() {
-    return(
-        <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButtoon to="/newproject" text="Criar Projeto" />
+            <img src={savings} alt="Costs" />
+        </section>
     )
 }
 
