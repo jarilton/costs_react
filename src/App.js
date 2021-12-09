e35f7e563de85bc0bdf7853dc9d8e222042179ff
Jarilton <jariltonpereira02@hotmail.com>
2021-12-09 13:58:21 -0300

    Página do formulário do projeto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
